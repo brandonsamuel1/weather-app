601037896dad6e661d9d1b5ff61c653ce10544b5
Brandon <brandonsamuel72@gmail.com>
2020-03-26 23:11:01 -0700

    added views and partials for dynamic content. added first route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
+const path = require('path')
 const express = require('express')
 const request = require('request')
-
+const hbs = require('hbs')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+
+app.use(express.static('public'))
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.set('view engine', 'hbs')
+hbs.registerPartials(path.join(__dirname, '../views/partials'))
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Brandon Samuel'
+    })
+})
 
 app.listen(PORT, () => { console.log(`Server started on port ${PORT}`) })
